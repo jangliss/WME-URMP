5ffa15aeeeb213b22e90da6a108eacb4d7170be6
Jon Angliss <jon+gh@netdork.net>
2023-08-05 17:43:15 -0500

    Add in some tweaks for beta/prod releases

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,51 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
-      dist: {
+      dev: {
         src: ['./src/header.js', './src/downloadHelper.js', './src/storageHelper.js', './src/icons.js', './src/dbHelper.js', './src/main.js'],
-        dest: 'dist/<%= pkg.main %>'
+        dest: 'dist/<%= pkg.main %>-beta.user.js'
+      },
+      build: {
+        src: ['./src/header.js', './src/downloadHelper.js', './src/storageHelper.js', './src/icons.js', './src/dbHelper.js', './src/main.js'],
+        dest: 'dist/<%= pkg.main %>.user.js'
       }
     },
     'string-replace': {
-      source: {
+      dev: {
         files: {
-          'dist/<%= pkg.main %>': 'dist/<%= pkg.main %>'
+          'dist/<%= pkg.main %>-beta.user.js': 'dist/<%= pkg.main %>-beta.user.js'
         },
         options: {
           replacements: [{
             pattern: /{{ VERSION }}/g,
             replacement: '<%= pkg.version %>'
+          },
+          {
+            pattern: /{{ BASE_URI }}/g,
+            replacement: 'beta'
+          },
+          {
+            pattern: /{{ RELEASE }}/g,
+            replacement: ' Beta'
+          }]
+        }
+      },
+      build: {
+        files: {
+          'dist/<%= pkg.main %>.user.js': 'dist/<%= pkg.main %>.user.js'
+        },
+        options: {
+          replacements: [{
+            pattern: /{{ VERSION }}/g,
+            replacement: '<%= pkg.version %>'
+          },
+          {
+            pattern: /{{ BASE_URI }}/g,
+            replacement: 'www'
+          },
+          {
+            pattern: /{{ RELEASE }}/g,
+            replacement: ''
           }]
         }
       }
@@ -25,5 +56,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat')
   grunt.loadNpmTasks('grunt-string-replace')
 
-  grunt.registerTask('default', ['concat', 'string-replace'])
+  grunt.registerTask('default', ['concat:build', 'string-replace:build'])
+  grunt.registerTask('beta', ['concat:dev', 'string-replace:dev'])
 }
